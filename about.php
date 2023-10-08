4d080dd8747264b41f25eba9711667f3f43cecb0
Laurin49 <welter@diwe.info>
2023-10-08 16:29:39 +0200

    Section 2 Abs 14

---
+++ about.php
@@ -0,0 +1,7 @@
+<?php
+
+require('function.php');
+
+$heading = 'About us';
+
+require "views/about.view.php";
---
+++ function.php
@@ -0,0 +1,5 @@
+<?php
+
+function urlIs($value): bool {
+    return $_SERVER['REQUEST_URI'] === $value;
+}
